4599450c41aeaee449a77253e4158f36e2057472
MiguelWainst <miguel.wainst@gmail.com>
2025-11-04 20:07:51 -0300

    Adiciona classes para gerenciamento de chat, incluindo envio de mensagens, histórico, notificações e persistência

---
+++ Exercicio4/NotificadorContato.java
@@ -0,0 +1,6 @@
+// classe que envia notificações pro contato
+class NotificadorContato {
+    public void notificarContato(String contato) {
+        System.out.println("notificando contato: " + contato)
+    }
+}
